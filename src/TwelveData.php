5a217f64f23647d22843ef89db169b045f026f20
Marek Skopal <skopal.marek@gmail.com>
2024-02-09 16:21:24 +0100

    Fix missing reference data

--- src/TwelveData.php
+++ src/TwelveData.php
@@ -5,17 +5,31 @@ declare(strict_types=1);
 namespace MarekSkopal\TwelveData;
 
 use MarekSkopal\TwelveData\Api\CoreData;
+use MarekSkopal\TwelveData\Api\ReferenceData;
 use MarekSkopal\TwelveData\Client\Client;
 
 class TwelveData
 {
     private readonly Client $client;
 
+    private ReferenceData $referenceData;
+
     private CoreData $coreData;
 
     public function __construct(string $apiKey)
     {
         $this->client = new Client($apiKey);
+    }
+
+    public function getReferenceData(): ReferenceData
+    {
+        if (isset($this->referenceData)) {
+            return $this->referenceData;
+        }
+
+        $this->referenceData = new ReferenceData($this->client);
+
+        return $this->referenceData;
     }
 
     public function getCoreData(): CoreData
