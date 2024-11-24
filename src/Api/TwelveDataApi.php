f1946006081df9b3ee4ecaa07b1fb43d2720570f
Marek Skopal <skopal.marek@gmail.com>
2024-11-24 09:34:14 +0100

    Add static json response tests

--- src/Api/TwelveDataApi.php
+++ src/Api/TwelveDataApi.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace MarekSkopal\TwelveData\Api;
 
-use MarekSkopal\TwelveData\Client\Client;
+use MarekSkopal\TwelveData\Client\ClientInterface;
 
 abstract class TwelveDataApi
 {
-    public function __construct(protected readonly Client $client)
+    public function __construct(protected readonly ClientInterface $client)
     {
     }
 }
